8ddfa6f710aa02861b620a8301d85685f6763105
shhra <29188967+shhra@users.noreply.github.com>
2022-03-12 19:03:14 +0100

    Setup main skeleton to get started.

--- src/base.hpp
+++ src/base.hpp
@@ -1,6 +1,36 @@
 #ifndef BASE_H_
 #define BASE_H_
 
+#include <vulkan/vulkan.h>
 
+// Report and handle the error
+#include <iostream>
+#include <stdexcept>
+
+// Get the EXIT_SUCCESS and EXIT_FAILURE macros
+#include <cstdlib>
+
+// This is a typical vulkan application where vulkan objects are stored.
+// The code is straight forward that provides function to load the bare
+// vulkan objects.
+class VkApp {
+
+public:
+  void run() {
+    init();
+    loop();
+    clean();
+  }
+
+private:
+  // Allocate resources
+  void init() {}
+
+  // Update the graphical elements.
+  void loop() {}
+
+  // Free the allocated resources
+  void clean() {}
+};
 
 #endif // BASE_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,22 @@
+#include <cstdlib>
+#include <exception>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include "base.hpp"
 
 #define GLFW_FORCE_RADIANS
 #include <iostream>
 
 int main() {
-  glfwInit();
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "explorer", nullptr, nullptr);
-
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-  std::cout << extensionCount << " extensions supported\n";
-
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+  VkApp app;
+  try {
+    app.run();
+  } catch (std::exception& e) {
+    // As soon as we except any fatal error, print it out.
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
   }
-
-  glfwDestroyWindow(window);
-
-  glfwTerminate();
-
-  return 0;
+  return EXIT_SUCCESS;
 }
